291de4e2a05480a1e14ef83f14b83cba46483d72
Maria Chernomazova <chernomazova@mari.bk>
2023-08-06 23:46:39 +0500

    Решили первую задачу

---
+++ zd_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте двумерный массив размером m*n, заполненный случайными вещественными числами, округленными до одного знака
+Console.WriteLine("Введите m");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите n");
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] a = new double[m,n];
+for (int i = 0; i< m; i++) {
+    for (int j = 0; j < n; j++) {
+        a[i,j] = Math.Round(Convert.ToDouble(Console.ReadLine()),1);
+    }
+}
+Console.WriteLine("Получившийся массив");
+for(int i=0; i<m;i++) {
+    for (int j = 0;j<n; j++) {
+        Console.Write("{0}\t",a[i,j]);
+    }
+    Console.WriteLine();
+}
